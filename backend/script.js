156e76d04eb93f2367bede14a31392149ec6d9b8
Adeola Ikuesan <adeola.ikuesan@gmail.com>
2023-01-18 21:20:34 +0200

    Script.js changed

--- backend/script.js
+++ backend/script.js
@@ -1,22 +1,29 @@
-const axios = require('axios');
+// const axios = require("axios");
+const droneController = require("../controllers/droneController");
 
 module.exports = callScript = () => {
+  setInterval(() => {
+    droneController.getAll();
+    droneController.deletePilot();
+  }, 2000);
 
-    setInterval(() => {
-        axios.get(`http://localhost:${process.env.PORT}`)
-            .then(response => {
-                // console.log(response.data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    
-        axios.post(`http://localhost:${process.env.PORT}/delete`)
-            .then(response => {
-                // console.log(response.data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }, 2000);
+//   setInterval(() => {
+//     axios
+//       .get(`http://localhost:${process.env.PORT}`)
+//       .then((response) => {
+//         // console.log(response.data);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+
+//     axios
+//       .post(`http://localhost:${process.env.PORT}/delete`)
+//       .then((response) => {
+//         // console.log(response.data);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }, 2000);
 };
